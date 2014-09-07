5b7295a9d42a050ac60febcf8555f1ebb86bf69f
Cody Poll <CJPoll@gmail.com>
2014-09-06 22:53:52 -0600

    User factory is valid
    Added stubs for descriptions of user properties
    
    User email is required
    
    User password is required

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject {FactoryGirl.build(:user)}
+
+	it "has a valid factory" do
+		expect(subject).to be_valid
+	end
+
+	context "#email" do
+		it "is required" do
+			subject.email = nil
+			expect(subject).to_not be_valid
+		end
+	end
+
+	context "#password" do
+		it "is required" do
+			subject.password = nil
+			expect(subject).to_not be_valid
+		end
+	end
 end
