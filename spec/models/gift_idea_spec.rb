a48c6f5a893211ce93719fa14b969334fbd0eff0
Cody Poll <CJPoll@gmail.com>
2014-09-06 19:02:41 -0600

    Added stubs for descriptions of gift idea properties
    Gift idea requires a name
    
    Gift idea descriptions are required
    
    Gift idea urls are required

--- spec/models/gift_idea_spec.rb
+++ spec/models/gift_idea_spec.rb
@@ -14,7 +14,29 @@ require 'rails_helper'
 
 RSpec.describe GiftIdea, :type => :model do
 	subject {FactoryGirl.build(:gift_idea)}
+
 	it "has a valid factory" do
 		expect(subject).to be_valid
+	end
+
+	describe '#name' do
+		it "is required" do
+			subject.name = nil
+			expect(subject).to_not be_valid
+		end
+	end
+
+	describe '#description' do
+		it "is required" do
+			subject.description = nil
+			expect(subject).to_not be_valid
+		end
+	end
+
+	describe '#url' do
+		it "is required" do
+			subject.url = nil
+			expect(subject).to_not be_valid
+		end
 	end
 end
