51f90bbd1e815a8c5fe3edf1f54c81b8b4308c44
Cody Poll <CJPoll@gmail.com>
2014-09-07 22:05:15 -0600

    Made current_user an instance variable so it's available in step definitions

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,8 +1,8 @@
 Given(/^I am logged in$/) do
-	user = FactoryGirl.create(:user)
+	@user = FactoryGirl.create(:user)
 	visit new_user_session_path
-	fill_in :user_email, with: user.email
-	fill_in :user_password, with: user.password
+	fill_in :user_email, with: @user.email
+	fill_in :user_password, with: @user.password
 	click_on 'Log in'
 	expect(current_path).to eq(gift_ideas_path)
 end
