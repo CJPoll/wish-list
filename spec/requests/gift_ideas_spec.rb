ff5ed716f7865792b3723924325e3360651de99f
Cody Poll <CJPoll@gmail.com>
2014-09-07 20:10:29 -0600

    gift_ideas spec is working correctly

--- spec/requests/gift_ideas_spec.rb
+++ spec/requests/gift_ideas_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "GiftIdeas", :type => :request do
-  describe "GET /gift_ideas" do
-    it "works! (now write some real specs)" do
-      get gift_ideas_path
-      expect(response.status).to be(200)
-    end
-  end
+	describe "GET /gift_ideas" do
+		it "redirects" do
+			get gift_ideas_path
+			expect(response.status).to be(302)
+		end
+	end
 end
