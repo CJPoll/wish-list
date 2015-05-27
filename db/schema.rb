0f64dd76083fb57503894b75cdf32a5dfe3d9bee
Cody Poll <cpoll@instructure.com>
2015-05-26 18:45:15 -0600

    The list shows the user's first name

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907090147) do
+ActiveRecord::Schema.define(version: 20150527002517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "gift_ideas", force: true do |t|
+  create_table "gift_ideas", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "url"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20140907090147) do
 
   add_index "gift_ideas", ["user_id"], name: "index_gift_ideas_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20140907090147) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
